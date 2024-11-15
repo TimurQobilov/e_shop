1537c9b18a9fa68b02f5ef58d4605af5ac049c6c
Developer <46593850+timurqobilov@users.noreply.github.com>
2024-11-16 01:02:21 +0500

    новқй файл urls

--- eshopapp/views.py
+++ eshopapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product, CategoryProduct
 
 # Create your views here.
+def home_page(request):
+    products = Product.objects.all()
+
+    context = {'products': products}
+    return render(request, 'home.html', context)

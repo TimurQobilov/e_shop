9d3d66069d4f3ed6a42cf78bbc6d6f56057f01cd
TimurQobilov <qobilovtemur.dev@gmail.com>
2024-11-16 19:06:53 +0500

    новқй файл html

--- eshop/settings.py
+++ eshop/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import STATIC_ROOT, STATICFILES_DIRS, MEDIA_URL, MEDIA_ROOT
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,6 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
+
+import os
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR / "assets", ), )
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- eshopapp/views.py
+++ eshopapp/views.py
@@ -4,6 +4,46 @@ from .models import Product, CategoryProduct
 # Create your views here.
 def home_page(request):
     products = Product.objects.all()
+    categories = CategoryProduct.objects.all()
 
-    context = {'products': products}
-    return render(request, 'home.html', context)
+    context = {'products': products, 'categories' : categories}
+
+    return render(request, 'index.html', context)
+
+
+def category_page(request, pk):
+    category = CategoryProduct.objects.get(id=pk)
+    products = Product.objects.filter(product_category=category).all()
+
+    context = {'products': products, "category" : category}
+
+    return render(request, 'category_product.html', context)
+
+def product_page(request, pk):
+    product = Product.objects.get(id=pk)
+
+    context = {'product': product}
+
+    return  render(request, 'product.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

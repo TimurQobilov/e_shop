8cde5b7c3f3e19d7a2defaa0c16bce0a014ce108
TimurQobilov <qobilovtemur.dev@gmail.com>
2024-11-16 19:06:53 +0500

    новқй файл html

--- eshopapp/views.py
+++ eshopapp/views.py
@@ -4,6 +4,46 @@ from .models import Product, CategoryProduct
 # Create your views here.
 def home_page(request):
     products = Product.objects.all()
+    categories = CategoryProduct.objects.all()
+
+    context = {'products': products, 'categories' : categories}
+
+    return render(request, 'home.html', context)
+
+
+def category_page(request, pk):
+    category = CategoryProduct.objects.get(id=pk)
+    products = Product.objects.filter(product_category=category)
 
     context = {'products': products}
-    return render(request, 'home.html', context)
+
+    return render(request, 'category.html', context)
+
+def product_page(request, pk):
+    product = Product.objects.get(id=pk)
+
+    context = {'product': product}
+
+    return  render(request, 'product.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

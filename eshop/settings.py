a7b77ff20cd607676cc100812df01e817b373342
TimurQobilov <qobilovtemur.dev@gmail.com>
2024-12-03 00:09:20 +0500

    add database postgres

--- eshop/settings.py
+++ eshop/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'e_shop_db',
+        'USER': 'postgres',
+        'PASSWORD': 'timur910210',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
